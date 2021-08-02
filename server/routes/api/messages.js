f1989fee69bcecb70f7e4fc4bd0c4252d7cebb1b
MashuqueH <47610989+MashuqueH@users.noreply.github.com>
2021-08-02 14:15:17 -0700

    fix message ordering and remove conversationId from requst

--- server/routes/api/messages.js
+++ server/routes/api/messages.js
@@ -4,37 +4,38 @@ const onlineUsers = require("../../onlineUsers");
 
 // expects {recipientId, text, conversationId } in body (conversationId will be null if no conversation exists yet)
 router.post("/", async (req, res, next) => {
-    try {
-        if (!req.user) {
-            return res.sendStatus(401);
-        }
-        const senderId = req.user.id;
-        const { recipientId, text, sender } = req.body;
-
-        let conversation = await Conversation.findConversation(
-            senderId,
-            recipientId
-        );
-
-        if (!conversation) {
-            // create conversation
-            conversation = await Conversation.create({
-                user1Id: senderId,
-                user2Id: recipientId,
-            });
-            if (onlineUsers.includes(sender.id)) {
-                sender.online = true;
-            }
-        }
-        const message = await Message.create({
-            senderId,
-            text,
-            conversationId: conversation.id,
-        });
-        res.json({ message, sender });
-    } catch (error) {
-        next(error);
+  try {
+    if (!req.user) {
+      return res.sendStatus(401);
     }
+
+    const senderId = req.user.id;
+    const { recipientId, text, sender } = req.body;
+
+    let conversation = await Conversation.findConversation(
+      senderId,
+      recipientId
+    );
+
+    if (!conversation) {
+      // create conversation
+      conversation = await Conversation.create({
+        user1Id: senderId,
+        user2Id: recipientId,
+      });
+      if (onlineUsers.includes(sender.id)) {
+        sender.online = true;
+      }
+    }
+    const message = await Message.create({
+      senderId,
+      text,
+      conversationId: conversation.id,
+    });
+    res.json({ message, sender });
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
